342f43e6ddd94e833131411e5cc103cdd60dedc3
Nathan Au-yeung <nhauyeung@berkeley.edu>
2026-02-11 17:10:58 -0800

    app.py

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 from flask import Flask, render_template
+import sqlite3
+app = Flask(__name__)
+
+con = sqlite3.connect("players_20.db", check_same_thread=False)
+cur = con.cursor()
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,3 +14,12 @@ def w209():
 
 if __name__ == '__main__':
     app.run()
+
+@app.route("/api")
+def api():
+    return {'x':2}
+
+@app.route("/players/count")
+def getPlayerCount():
+    res = cur.execute("SELECT COUNT(*) as ct FROM users")
+    return {"count": res.fetchone()[0]}
